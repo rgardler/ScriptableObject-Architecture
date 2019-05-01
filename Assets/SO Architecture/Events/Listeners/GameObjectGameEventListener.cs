cab394d519d19311845a55aaa948fbf4439a6d8e
Daniel Everland <danieleverland@gmail.com>
2019-05-01 16:57:33 +0200

    Purged repo

--- Assets/SO Architecture/Events/Listeners/GameObjectGameEventListener.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace ScriptableObjectArchitecture
-{
-    [AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "GameObject")]
-    public sealed class GameObjectGameEventListener : BaseGameEventListener<GameObject, GameObjectGameEvent, GameObjectUnityEvent>
-    {
-    }
-}
--- Assets/SO Architecture/Events/Listeners/ObjectGameEventListener.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace ScriptableObjectArchitecture
-{
-    [AddComponentMenu(SOArchitecture_Utility.EVENT_LISTENER_SUBMENU + "Object")]
-    public class ObjectGameEventListener : BaseGameEventListener<Object, ObjectGameEvent, ObjectUnityEvent>
-    {
-    }
-}
